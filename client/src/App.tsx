b68728a8d194dad9e05c259a7ba654faf28e9418
Shahil Jha <shahilkumar0511@gmail.com>
2025-07-05 18:12:08 +0530

    Refactored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Hero from "./page/Hero"
-import EditorDashboard from "./page/EditorDashboard"
-import AdminDashboard from "./page/AdminDashboard"
-import Dashboard from "./page/Dashboard"
-import ProtectedRoute from "./routes/ProtectedRoutes"
-import Post from "./page/Post"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Hero from "./page/Hero";
+import EditorDashboard from "./page/EditorDashboard";
+import AdminDashboard from "./page/AdminDashboard";
+import Dashboard from "./page/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoutes";
+import Post from "./page/Post";
+import { useAppSelector } from "./app/hooks";
+import { selectIsAuthenticated, selectRole } from "./features/auth/authSlice";
 
 function App() {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  const role = useAppSelector(selectRole);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hero />} />
+        <Route
+          path="/"
+          element={isAuthenticated ? (
+            role === 'admin' ? <Navigate to="/admin" /> :
+              role === 'editor' ? <Navigate to="/editor" /> :
+                <Navigate to="/user" />
+          ) : (
+            <Hero />
+          )}
+        />
         <Route element={<ProtectedRoute allowedRoles={['editor']} />}>
           <Route path="/editor" element={<EditorDashboard />} />
         </Route>
@@ -24,10 +37,9 @@ function App() {
         <Route element={<ProtectedRoute allowedRoles={['admin', 'editor']} />}>
           <Route path="/post" element={<Post />} />
         </Route>
-
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
